d8bad2bc7df5b59aa1ef3bbe8c1b346727c79551
Konstantin Krivilev <inferno750@mail.ru>
2024-12-13 00:22:22 +0300

    init typescript

--- src/app/js/main.ts
+++ src/app/js/main.ts
@@ -5,20 +5,44 @@ import { initPhoneMask } from './helpers/initPhoneMask';
 import { handleValidation } from './helpers/validate';
 import { handleSearchModal } from './modal/handleSearchModal';
 
-const btnAddNode = document.getElementById('js-btn-add');
-const btnClearNode = document.getElementById('js-btn-clear');
-const btnSearchNode = document.getElementById('js-btn-search');
-const nameNode = document.getElementById('js-input-name');
-const vacancyNode = document.getElementById('js-input-vacancy');
-const phoneNode = document.getElementById('js-input-phone');
-const contactsCardNodes = document.querySelectorAll('.contacts__card');
+const btnAddNode = document.getElementById(
+    'js-btn-add',
+) as HTMLButtonElement | null;
+const btnClearNode = document.getElementById(
+    'js-btn-clear',
+) as HTMLButtonElement | null;
+const btnSearchNode = document.getElementById(
+    'js-btn-search',
+) as HTMLButtonElement | null;
+const nameNode = document.getElementById(
+    'js-input-name',
+) as HTMLInputElement | null;
+const vacancyNode = document.getElementById(
+    'js-input-vacancy',
+) as HTMLInputElement | null;
+const phoneNode = document.getElementById(
+    'js-input-phone',
+) as HTMLInputElement | null;
+const contactsCardNodes =
+    document.querySelectorAll<HTMLElement>('.contacts__card');
+
+interface IContact {
+    name: string;
+    vacancy: string;
+    phone: string;
+}
 
 loadContacts();
 initPhoneMask(phoneNode);
 initContactsCardZIndex(contactsCardNodes);
 
-btnAddNode.addEventListener('click', () => {
-    const contact = {
+btnAddNode?.addEventListener('click', () => {
+    if (!nameNode || !vacancyNode || !phoneNode) {
+        console.error('Input nodes are missing');
+        return;
+    }
+
+    const contact: IContact = {
         name: nameNode.value,
         vacancy: vacancyNode.value,
         phone: phoneNode.value,
@@ -39,10 +63,12 @@ btnAddNode.addEventListener('click', () => {
     clearInputs(nameNode, vacancyNode, phoneNode);
 });
 
-btnClearNode.addEventListener('click', () => {
+btnClearNode?.addEventListener('click', () => {
     contactsCardNodes.forEach((node) => {
-        const infoNode = node.querySelector('.card__info');
-        const numNode = node.querySelector('.card__num');
+        const infoNode = node.querySelector(
+            '.card__info',
+        ) as HTMLElement | null;
+        const numNode = node.querySelector('.card__num') as HTMLElement | null;
 
         if (infoNode && numNode) {
             infoNode.remove();
@@ -54,13 +80,13 @@ btnClearNode.addEventListener('click', () => {
     localStorage.removeItem('contacts');
 });
 
-btnSearchNode.addEventListener('click', () => {
+btnSearchNode?.addEventListener('click', () => {
     handleSearchModal();
 });
 
 contactsCardNodes.forEach((node) => {
     const cardContainerNode = node.querySelector('.card__container');
-    cardContainerNode.addEventListener('click', () => {
+    cardContainerNode?.addEventListener('click', () => {
         node.classList.toggle('show');
     });
 });
